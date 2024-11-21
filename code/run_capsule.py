b0bfc74edab5bf5c3e02b1926bbd38af0ee947d3
David Feng <david.feng@alleninstitute.org>
2024-11-21 19:14:14 +0000

    typo

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -88,7 +88,7 @@ def write_output_metadata(
         derived_dd = DerivedDataDescription.from_data_description(
             data_description=new_dd,
             process_name="processed"
-
+        )
         derived_dd.write_standard_file(output_directory=Path(output_fp).parent)
 
 
